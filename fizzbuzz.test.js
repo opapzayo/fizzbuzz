07f340f0f3812945dd24fc56f8c5e9b31ab2451e
opapzayo <apsiwagorn@hotmail.com>
2019-09-21 15:37:56 +0700

    fizzbuzz t

---
+++ fizzbuzz.test.js
@@ -0,0 +1,10 @@
+const FizzBuzz = require("./fizzbuzz");
+
+describe("Test FizzBuzz", () => {
+  test("Test 1 expect 1", () => {
+    const result = FizzBuzz.say(1);
+    expect(result).toEqual(1); //Assert Function
+  });
+});
+
+//npm install -g jest
